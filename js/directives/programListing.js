25508c2f7cc881d92a11fe433d380617654dcb43
Salma Mohammed <sm.71175@gmail.com>
2018-08-16 10:47:26 +0200

    create new directive programListing

---
+++ js/directives/programListing.js
@@ -0,0 +1,12 @@
+app.directive("programListing", function () {
+	// body...
+	return {
+		restrict: 'E',
+		scope: {
+			info: '='
+		},
+		templateUrl: 'js/directives/programListing.html'
+	}
+	
+
+})
